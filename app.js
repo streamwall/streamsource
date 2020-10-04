3ff5dd09f9199572d686ab360088864905cf0289
Ben Menesini <ben.menesini@rover.com>
2020-10-03 23:59:31 -0700

    Add LogDNA as logging provider

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const logger = require('./middleware/logger');
 const bodyParser = require('body-parser')
 const boolParser = require('express-query-boolean');
 
@@ -23,7 +23,7 @@ app.use(boolParser())
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+app.use(logger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
